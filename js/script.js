9955884dde28be5046cfaa905c2f59c9917a438c
Athena <athena.maquelyn@gmail.com>
2017-03-12 14:42:42 -0500

    Menu styles are a go

--- js/script.js
+++ js/script.js
@@ -3,6 +3,20 @@ $(document).ready(function(){
 	// 	$('.btn').addClass('pulse');
 	// })
 
+	var scroll_start = 0;
+	   var startchange = $('#startchange');
+	   var offset = startchange.offset();
+	    if (startchange.length){
+	   $(document).scroll(function() { 
+	      scroll_start = $(this).scrollTop();
+	      if(scroll_start > offset.top) {
+	          $(".navbar-default").css('background-color', '#000000');
+	       } else {
+	          $('.navbar-default').css('background-color', 'transparent');
+	       }
+	   });
+	    }
+
 	$( ".btn" ).hover(function() {
 	  $( this ).addClass('pulse');
 	});
